334ccdf6ab751d647641355a29e6fe72ab2028aa
Divyansh Bhardwaj <divyansh.bhardwaj@bobble.ai>
2025-09-05 12:15:49 +0530

    Initial Commit

---
+++ Dictation-Daddy/Dictation_DaddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dictation_DaddyApp.swift
+//  Dictation-Daddy
+//
+//  Created by MacBook Pro on 05/09/25.
+//
+
+import SwiftUI
+
+@main
+struct Dictation_DaddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
